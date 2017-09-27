889b54ffcc14efd8f2e9e722bf89d93464a22139
Dusk Eagle <Dusk.Eagle@gmail.com>
2017-09-27 01:13:53 -0700

    Initial working commit

--- src/main/scala/com/duskeagle/resttest/RestTest.scala
+++ src/main/scala/com/duskeagle/resttest/RestTest.scala
@@ -2,14 +2,20 @@ package com.duskeagle.resttest
 
 object RestTest {
 
+  def printBalanceSummary(tm: TransactionManager): Unit = {
+    tm.runningBalance.foreach { case (date, balance) =>
+      println(s"$date\t$$$balance")
+    }
+    println(s"Total\t$$${tm.totalBalance}")
+  }
+
   def main(args: Array[String]): Unit = {
     val apiRequest = args.toList match {
       case Nil => ApiRequest.apply
       case endpoint :: Nil => ApiRequest(endpoint)
       case _ => sys.error("Usage: ./resttest [alternative endpoint]")
     }
-    val transactionManager = new TransactionManager(apiRequest.getTransactions())
-    println(transactionManager.totalBalance)
+    printBalanceSummary(new TransactionManager(apiRequest.getTransactions()))
   }
 
 }
